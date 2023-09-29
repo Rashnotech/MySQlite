53986988ad7560d7eed14d9658e5b42acb9183c9
Rashnotech <rashnotech@gmail.com>
2023-09-29 01:30:55 +0000

    entry point - main

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "header.h"
+/**
+ * main - entry point of program
+ * argc: argument counter
+ * argv: argument vector
+ * Return: an integer value of 1 otherwise 0
+ */
+int main(int argc, char **argv)
+{
+	return (1);
+}
